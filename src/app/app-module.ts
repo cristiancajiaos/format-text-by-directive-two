2717aee7ff9512a18f744ac4327f736504b6272f
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-07-30 17:16:29 -0400

    Creado layout

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,14 +3,22 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Layout } from './components/layout/layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Layout
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
---
+++ src/app/components/layout/layout.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-layout',
+  standalone: false,
+  templateUrl: './layout.html',
+  styleUrl: './layout.scss'
+})
+export class Layout implements OnInit {
+
+  constructor(
+    private toastr: ToastrService
+  ) {
+
+  }
+  
+  ngOnInit(): void {
+
+  }
+
+  public pressMe(): void {
+    this.toastr.success('Thanks! :)');
+  }
+
+}
